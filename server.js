cc39f65719fcc2452d26b2fdec6ad9369221ff82
Timpan5 <tim.pan@mail.utoronto.ca>
2016-09-13 12:52:06 -0400

    Comment style in server.js

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ var cheerio = require("cheerio");
 var cheerioTableparser = require('cheerio-tableparser');
 var rr = require("request");
 
-//Connect to PSQL database and establish client pool
-//URL gets reset when powershell closes
-//$env:DATABASE_URL = "postgres://trrfizoisrjhci:n-Zxst0BRii6nGB1h2oQ4OioM8@ec2-54-225-90-198.compute-1.amazonaws.com:5432/dspf3s3sfi2d6"
+/* 
+* Connect to PSQL database and establish client pool.
+* Connection information read from env variable.
+*/
 const Pool = require('pg-pool');
 var pgConfig = process.env.DATABASE_URL.split(":");
 const config = {
@@ -25,6 +26,10 @@ const config = {
 };
 var pool = new Pool(config);
 
+/* 
+* Setup Express.
+* Setup settings for parsing and path to static content.
+*/
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -32,12 +37,18 @@ app.use(bodyParser.urlencoded({
 })); 
 app.use(express.static(path.join(__dirname, "public")));
 
+/* 
+* Define port for server to listen on.
+* Port is obtained from env variable when possible.
+*/
 var port = process.env.PORT || 5000;
 var server = app.listen(port, function () {
 	console.log("Listening: " + server.address().port);
 });
 
-//Names of tables holding items in PSQL database
+/* 
+* Table names.
+*/
 const dbTableName = {
 	"wepMelee" : "weapons_melee",
 	"head" : "head",
@@ -52,6 +63,10 @@ const dbTableName = {
 	"ring" : "ring",
 };
 
+/* 
+* Send all table data to be used in datalist.
+* Or send data for specified table row.
+*/
 app.get(/(List)/, function (req, res) { 
 	var base = req.path;
 	var item = base.split("\/");
@@ -67,22 +82,20 @@ app.get(/(List)/, function (req, res) {
 		sendStats(name, res, getItemQuery(dbTableName[target]));
 	}
 	else {
-		console.log("List failed");
 		res.status(400).send("List failed");
 	}	
 });
 
+/* 
+* Send data for specified table.
+*/
 app.get(/(prayer)/, function (req, res) { 
 	var base = req.path;
 	var item = base.split("\/");
 	
-	if (item.length == 2) {
-		//sendlist
-	}
-	else if (item.length == 3) {
+	if (item.length == 3) {
 		var name = decodeURIComponent(item[2]);
 		var query = "SELECT * FROM prayer WHERE style=$1";
-		
 		pool.query(query, [name], function(err, result) {
 			var jsonObj = {"data" : result.rows};
 			if (!result.rows.length) {
@@ -93,25 +106,22 @@ app.get(/(prayer)/, function (req, res) {
 				res.send(jsonObj);
 			}
 		});
-		
 	}
 	else {
-		console.log("List failed");
 		res.status(400).send("List failed");
 	}	
 });
 
+/* 
+* Send data for specified table.
+*/
 app.get(/(potion)/, function (req, res) { 
 	var base = req.path;
 	var item = base.split("\/");
 	
-	if (item.length == 2) {
-		//sendlist
-	}
-	else if (item.length == 3) {
+	if (item.length == 3) {
 		var name = decodeURIComponent(item[2]);
 		var query = "SELECT * FROM potion WHERE style=$1 ORDER BY percentage ASC";
-		
 		pool.query(query, [name], function(err, result) {
 			var jsonObj = {"data" : result.rows};
 			if (!result.rows.length) {
@@ -122,36 +132,46 @@ app.get(/(potion)/, function (req, res) {
 				res.send(jsonObj);
 			}
 		});
-		
 	}
 	else {
-		console.log("List failed");
 		res.status(400).send("List failed");
 	}	
 });
 
+/* 
+* Calculate melee roll from information parsed from client POST request.
+* This calculation is a part of the combat formula.
+*/
 app.post("/calculate/roll/melee", function (req, res) {
 	var input = req.body;
-	console.log(input);
-	
 	var roll = maxRoll(parseInt(input.visible), parseFloat(input.pAcc), parseInt(input.style), parseFloat(input.v), parseInt(input.bonus), parseFloat(input.gear));
 	res.send({roll});
 });
 
+/* 
+* Calculate melee max from information parsed from client POST request.
+* This calculation is a part of the combat formula.
+*/
 app.post("/calculate/hit/melee", function (req, res) {
 	var input = req.body;
-	console.log(input);
 	var hit = maxHit(parseInt(input.visible), parseFloat(input.pStr), parseInt(input.style), parseFloat(input.v), parseInt(input.bonus), parseFloat(input.gear));
 	res.send({hit});
 });
 
+/* 
+* Calculate range max from information parsed from client POST request.
+* This calculation is a part of the combat formula.
+*/
 app.post("/calculate/hit/range", function (req, res) {
 	var input = req.body;
-	console.log(input);
 	var hit = maxHitRange(parseInt(input.visible), parseFloat(input.pStr), parseInt(input.style), parseFloat(input.v), parseInt(input.bonus), parseFloat(input.gear));
 	res.send({hit});
 });
 
+/* 
+* Send all NPC table data to be used in datalist.
+* Or send data for specified table row.
+*/
 app.get(/(npc)/, function(req, res) {
 	var base = req.path;
 	var item = base.split("\/");
@@ -174,7 +194,6 @@ app.get(/(npc)/, function(req, res) {
 		var name = decodeURIComponent(item[2]);
 		var query = "SELECT * FROM npc WHERE name=$1";
 		var queryName = "npc_name";
-		
 		pool.query({name : queryName, text : query, values : [name]}, function(err, result) {
 			var jsonObj = {"data" : result.rows};
 			if (!result.rows.length) {
@@ -183,55 +202,63 @@ app.get(/(npc)/, function(req, res) {
 			}
 			else {
 				res.send(jsonObj);
-				//console.log(jsonObj.data);
 			}
 		});
-		
 	}
 	else {
 		console.log("NPC failed");
 		res.status(400).send("NPC failed");
-	}	
-
-	
+	}
 });
 
+/* 
+* Calculate hit chance from information parsed from client POST request.
+* This calculation is a part of the combat formula.
+*/
 app.post("/calculate/chance", function (req, res) {
 	var input = req.body;
-	console.log(input);
 	var chance = hitChance(parseInt(input.A), parseInt(input.B));
-	console.log(chance);
 	res.send({chance});
 });
 
+/* 
+* Send spell table data to client.
+*/
 app.get("/spells", function(req, res) {
-		var query = "SELECT * FROM spell";
-		
-		pool.query(query, function(err, result) {
-			var jsonObj = {"data" : result.rows};
-			if (!result.rows.length) {
-				res.statusMessage = "Item not found";
-				res.status(400).end();
-			}
-			else {
-				res.send(jsonObj);
-			}
-		});
+	var query = "SELECT * FROM spell";
+	pool.query(query, function(err, result) {
+		var jsonObj = {"data" : result.rows};
+		if (!result.rows.length) {
+			res.statusMessage = "Item not found";
+			res.status(400).end();
+		}
+		else {
+			res.send(jsonObj);
+		}
+	});
 });
 
-
-app.get("*", function(req, res) {
-	console.log("*: " + req.path);
-});
-
+/**
+* Setup the query to use for searching a table.
+* @param {string} tableName - Name of the table to search.
+ */
 function getItemQuery(tableName) {
 	return "SELECT * FROM " + tableName + " WHERE name=$1";
 }
 
+/**
+* Setup the query to use for displaying names in a table.
+* @param {string} tableName - Name of the table to display.
+ */
 function getListQuery(tableName) {
 	return "SELECT name FROM " + tableName + " ORDER BY name ASC";
 }
 
+/**
+* Send list of names from table to client, to be used in a datalist.
+* @param {object} res - Response object.
+* @param {string} query - String to use in PSQL query.
+ */
 function sendList(res, query) {
 	pool.query(query, function(err, result) {
 		var jsonObj = {"data" : result.rows};
@@ -239,11 +266,16 @@ function sendList(res, query) {
 	});
 }
 
+/**
+* Send item stats for a specified item.
+* @param {string} name - Name of item to return data for.
+* @param {object} res - Response object.
+* @param {string} query - String to use in PSQL query.
+ */
 function sendStats(name, res, query) {
 	if (/[^a-z\s\d\(\)']/gi.test(name)) { 
 		res.status(400).send("Invalid item name");
 	}
-	
 	else {
 		pool.query(query, [name], function(err, result) {
 			var jsonObj = {"data" : result.rows};
@@ -258,18 +290,19 @@ function sendStats(name, res, query) {
 	}
 }
 
+/**
+* Send NPC stats for a specified NPC.
+* @param {string} id - id of the NPC being scraped.
+* @param {string} title - Name of the NPC.
+* @param {string} page - URL of the page for this NPC.
+ */
 function getNpcStats(id, title, page) {
 	rr({url : page}, function (error, res, body) {
 		if (!error && res.statusCode == 200) {
 			$ = cheerio.load(body);
-
 			$("table table").each(function() {
 				var row = $("tr", this).text();
-				
 				if (row.indexOf("Combat info") != -1) {
-					
-					console.log(id + " " + title);
-					
 					var hp = row.match(/(Hitpoints)[\s]*[\d]*/g);
 					var maxHp = 0;
 					if (hp[0].match(/([\d]+)/) != null)
@@ -283,8 +316,7 @@ function getNpcStats(id, title, page) {
 					
 					var defense = row.match(/(Defensive stats[\s\S]*Other bonuses)/g);
 					var defenseStats = defense[0].match(/([^a-zA-Z \n]*)/g);
-					
-					//hardcoded table name
+
 					pool.query('INSERT INTO npc VALUES ($1, $2, $3, $4, $5, $6)', [id, title, maxHp, npcStats(baseStats), npcStats(offenseStats), npcStats(defenseStats)], function(err) {});
 				}
 			});
@@ -292,22 +324,33 @@ function getNpcStats(id, title, page) {
 	});
 }
 
+/**
+* Parse NPC stats into individual integers.
+* @param {string} stats - Input string which will be parsed.
+ */
 function npcStats(stats) {
 	var len = stats.length;
-	
 	for (i = 0; i < len; i++)
 		if (stats[i] != "")
 			stats.push(stats[i].replace(/\s+/g, ''));
 	
 	stats.splice(0, len);
-	
 	if (stats[stats.length - 1] == "") {
 		stats.pop();
 	}
-	
 	return stats;
 }
 
+/**
+* Calculate and return max hit with the given inputs.
+* This is a part of the combat formula.
+* @param {integer} visible - Base visible value.
+* @param {float} prayer - Bonus to base value.
+* @param {integer} Stance - Stance bonus to visible value.
+* @param {float} v - Void bonus.
+* @param {integer} B - Total equipment stats.
+* @param {float} gear - Special equipment bonuses.
+ */
 function maxHit(visible, prayer, stance, v, B, gear) {
 	var step1 = Math.floor(visible * prayer);
 	var step2 = step1 + stance + 8;
@@ -328,6 +371,16 @@ function maxHit(visible, prayer, stance, v, B, gear) {
 	return maxHit;
 }
 
+/**
+* Calculate and return range max hit with the given inputs.
+* This is a part of the combat formula.
+* @param {integer} visible - Base visible value.
+* @param {float} prayer - Bonus to base value.
+* @param {integer} Stance - Stance bonus to visible value.
+* @param {float} v - Void bonus.
+* @param {integer} B - Total equipment stats.
+* @param {float} gear - Special equipment bonuses.
+ */
 function maxHitRange(visible, prayer, stance, v, B, gear) {
 	var step1 = Math.floor(visible * prayer);
 	var step2 = step1 + stance + 8;
@@ -348,6 +401,12 @@ function maxHitRange(visible, prayer, stance, v, B, gear) {
 	return maxHit;
 }
 
+/**
+* Calculate and return hir chance with the given inputs.
+* This is a part of the combat formula.
+* @param {integer} rollA - Accuracy roll value.
+* @param {integer} rollD - Defense roll value.
+ */
 function hitChance(rollA, rollD) {
 	var chance = 0;
 	if (rollA > rollD) {
@@ -359,56 +418,42 @@ function hitChance(rollA, rollD) {
 	return chance;
 }
 
+/**
+* Calculate and return accuracy roll with the given inputs.
+* This is a part of the combat formula.
+* @param {integer} visible - Base visible value.
+* @param {float} prayer - Bonus to base value.
+* @param {integer} Stance - Stance bonus to visible value.
+* @param {float} v - Void bonus.
+* @param {integer} B - Total equipment stats.
+* @param {float} gear - Special equipment bonuses.
+ */
 function maxRoll(visible, prayer, stance, v, B, gear) {
-
 	var step1 = Math.floor(visible * prayer); 
 	var step2 = step1 + stance + 8;
-	
-	console.log("1: " + step2);
-	
 	var A = Math.floor(step2 * v);
-	
 	var maxRollBase = A * (B + 64);
-	
-	console.log("2: " + maxRollBase);
-	
 	var step3 = Math.floor(maxRollBase * gear);
-	var special = 1; //weapon
-	
-	var maxRoll = Math.floor(step3 * special);
-	
-	console.log("3: " + maxRoll);
-	return maxRoll;
+	return step3;
 }
 
 function getStats(id, title, page) {
 	rr({url : page}, function (error, res, body) {
 		if (!error && res.statusCode == 200) {
 			$ = cheerio.load(body);
-
 			$("table").each(function() {
 				var row = $('tr', this).text();
-				
 				if (row.indexOf("Attack bonus") != -1) {
 					var pattern = /([\+\-]?\d+\%?)/g;
 					var stats = row.substr(row.indexOf("Attack bonus")).match(pattern);
-					
 					var speed = 0;
 					$("tr th span img", this).each(function() {
 						if (this["attribs"]["alt"].match(/\d/)) {
 							speed = this["attribs"]["alt"].match(/\d/)[0];
 						}
 					});
-					
-					//hardcoded table name
 					pool.query('INSERT INTO weapons_melee VALUES ($1, $2, $3, $4)', [id, title, stats, speed], function(err) {console.log("ERROR " + title);});
 					console.log(id);
-					/*
-					console.log(title);
-					console.log(stats);
-					console.log(speed);
-					*/
-					
 				}
 			});
 		};
